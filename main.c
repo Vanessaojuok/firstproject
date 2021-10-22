197007f5ce5830ca69fbb244e03c53e29923e885
vanessa <vanessaojuok2000@gmail.com>
2021-10-22 12:19:10 +0300

    added 3 lines for adding two numbers

--- main.c
+++ main.c
@@ -4,6 +4,10 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
+	int a=2, b=3;
+	int sum;
 	printf("Hello World");
+	sum = a +b;
+	printf("%d", sum);
 	return 0;
 }
